34b152847abbaf3651da884212e5a1b30f985ef9
aniekmarkus <a.markus@erasmusmc.nl>
2024-07-23 10:42:38 +0200

    Fix building R package; 'no template named unary function'

--- src/C++/Explore/set.cpp
+++ src/C++/Explore/set.cpp
@@ -423,7 +423,7 @@ string SET::PrintPerformance() {
   return Result.str();
 }
 
-struct AndJibu : public std ::unary_function<int, void>
+struct AndJibu : public std ::__unary_function<int, void>
 {
 	const boost::dynamic_bitset<> Source;
 	boost::dynamic_bitset<> Dest;
