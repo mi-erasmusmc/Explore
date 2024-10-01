47a3237bbc593d771f9d6d65b63e08f74f8d9b1c
Cesar Barboza <c.barbozagutierrez@erasmusmc.nl>
2024-10-01 13:15:57 +0200

    test correction

--- tests/testthat/test-HelperFunctions.R
+++ tests/testthat/test-HelperFunctions.R
@@ -7,11 +7,7 @@ test_that("Convert logical to 0/1", {
                            `316139209` = c(FALSE,FALSE,FALSE,
                                            FALSE,FALSE,FALSE),
                            `316139210` = c(FALSE,FALSE,FALSE,
-                                           FALSE,FALSE,FALSE)
-  )
-  
+                                           FALSE,FALSE,FALSE))
   train_data <- convert_logical(train_data)
-  
   expect_true(all(sapply(train_data, function(col) all(col %in% c(0, 1)))))
-  
 })
